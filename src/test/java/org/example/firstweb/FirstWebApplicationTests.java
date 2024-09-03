9f24e4741a10e730ca60c9207480574c034349d8
Flumakashi <flumakashi@gmail.com>
2024-09-03 16:11:01 +0300

    creating app

---
+++ src/test/java/org/example/firstweb/FirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firstweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
